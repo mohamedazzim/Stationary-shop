f41748935b63da99c3a481c7c2395e79ea18fb46
mohamedazzim <azzimandabdullah1@gmail.com>
2023-06-30 21:25:29 +0530

    last update

---
+++ PRODUCT.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Stationary_Shop
+{
+    public class Product
+    {
+        int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+
+        string product;
+
+        public string Product1
+        {
+            get { return product; }
+            set { product = value; }
+        }
+        string category;
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        float price;
+
+        public float Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        string taxtype;
+
+        public string Taxtype
+        {
+            get { return taxtype; }
+            set { taxtype = value; }
+        }
+        string status;
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+        public override string ToString()
+        {
+            return string.Format(" Id : {0} \n Product : {1} \n Category : {2}  \n Price : {3} \n TaxType : {4} \n Status : {5} ", this.id, this.product, this.category, this.price, this.taxtype, this.status);
+        }
+
+        public Product(int Id, string product, string category, float price, string taxtype, string status)
+        {
+            this.id = Id;
+            this.product = product;
+            this.category = category;
+            this.price = price;
+            this.taxtype = taxtype;
+            this.status = status;
+        }
+
+        public Product()
+        {
+ 
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Stationary_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region INT Call
+            //string path = "E:\\Stationaryshop\\items.txt";
+            //string[] file = File.ReadAllLines(path);
+            //Product[] pr = new Product[file.Length];
+
+            //Dictionary<int, Product> dick = new Dictionary<int, Product>();
+
+            //for (int i = 0; i < file.Length; i++)
+            //{
+            //    string[] split = file[i].Split('|');
+            //    pr[i] = new Product();
+            //    pr[i].Id = int.Parse(split[0]);
+            //    pr[i].Product1 = split[1];
+            //    pr[i].Category = split[2];
+            //    pr[i].Price = float.Parse(split[3]);
+            //    pr[i].Taxtype = split[4];
+            //    pr[i].Status = split[5];
+            //    dick.Add(int.Parse(split[0]), pr[i]);
+            //}
+            //Console.WriteLine("Enter the Key : ");
+            //int key_ = int.Parse(Console.ReadLine());
+            //Product dummy = dick[key_];
+            //Console.WriteLine(dummy);
+            #endregion
+
+            #region Name Call 
+            //string path = "E:\\Stationaryshop\\items.txt";
+            //string[] file = File.ReadAllLines(path);
+            //Product[] pr = new Product[file.Length];
+
+            //Dictionary<string, Product> dick = new Dictionary<string, Product>();
+
+            //for (int i = 0; i < file.Length; i++)
+            //{
+            //    string[] split = file[i].Split('|');
+            //    pr[i] = new Product();
+            //    pr[i].Id = int.Parse(split[0]);
+            //    pr[i].Product1 = split[1];
+            //    pr[i].Category = split[2];
+            //    pr[i].Price = float.Parse(split[3]);
+            //    pr[i].Taxtype = split[4];
+            //    pr[i].Status = split[5];
+            //    dick.Add(split[1], pr[i]);
+            //}
+            //Console.WriteLine("Enter the Product_Name : ");
+            //string name_ = Console.ReadLine();
+            //Product dummy_name = dick[name_];
+            //Console.WriteLine(dummy_name);
+            #endregion
+
+            string path = "E:\\Stationaryshop\\items.txt";
+            string[] file = File.ReadAllLines(path);
+            Product[] pr = new Product[file.Length];
+
+            //Dictionary<string, Product> dick = new Dictionary<string, Product>();
+
+            List<Product> list_name = new List<Product>();
+
+
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                string[] split = file[i].Split('|');
+                list_name.Add(new Product(int.Parse(split[0]),split[1], split[2], float.Parse(split[3]), split[4], split[5]));
+                //pr[i] = new Product();
+                //pr[i].Id = int.Parse(split[0]);
+                //pr[i].Product1 = split[1];
+                //pr[i].Category = split[2];
+                //pr[i].Price = float.Parse(split[3]);
+                //pr[i].Taxtype = split[4];
+                //pr[i].Status = split[5];
+                //dick.Add(split[1], pr[i]);
+            }
+            //Console.WriteLine("Enter the Product_Name : ");
+            //string name_ = Console.ReadLine();
+            //Product dummy_name = dick[name_];
+            //Console.WriteLine(dummy_name);
+
+            Console.WriteLine(list_name[0]);
+        }
+    }
+}
